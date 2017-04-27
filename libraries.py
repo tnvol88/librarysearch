56eeb459202d1dc9558e45124367fe86780ea110
tnvol88 <taylorbenjamin8@gmail.com>
2017-04-27 07:16:02 -0400

    Add files via upload

--- libraries.py
+++ libraries.py
@@ -1,28 +1,9 @@
-"""
-In order for this code to work, you will need to install the googleplaces and openpyxl libraries. I used pip to install both.
-You can find many tutorials on install each online. If you have questions, you can let me know. 
-
--Openpyxl will not allow the excel file to be open while the program is running or it throw an error. 
--You'll see as the program runs that I have search results printed to the console. I did for a couple reasons, one because
-the program takes a while to run and this provides feedback that it's continuing to function and also because you can see 
-how many results were returned in the search. 
--Because of the number of Google searches, this code could take a while to run.
-
-Googleplaces API:
-You will need to sign up to use google api services. The first level sign up includes 1000 queries. If you attach
-a credit card to the account, you will be permitted 150,000 queries. Google says you won't be charged for this level
-and it is just for verification. You can read more about that on the Googleplaces API page.
-
-You will need to edit code on lines 24,27, and 80 with your own information. I've left comments to the side of those lines
-exlaining what is needed.
-"""
-
 from googleplaces import GooglePlaces, types, lang
 from openpyxl import Workbook, load_workbook
 from openpyxl.styles import Color, PatternFill, colors
 
 
-API_KEY = '' #Enter your key between quotes
+API_KEY = 'AIzaSyDLdDH5kqx4-kYy3qlTpxjGc0KEVD9orN4' #Enter your key between quotes
 google_places = GooglePlaces(API_KEY)
 
 wb = load_workbook('library.xlsx')
@@ -39,13 +20,12 @@ greenFill = PatternFill('solid',fgColor='00FF00') #Found one exact match and is 
 yellowFill = PatternFill('solid',fgColor='FFFF00') #Did not find any matches
 cyanFill = PatternFill('solid',fgColor='00FFFF') #Found multiple results, none exact, is open
 magentaFill = PatternFill('solid',fgColor='FF00FF') #Found multiple results, none exact, is closed
+blueFill = PatternFill('solid',fgColor='0000FF') #Not exact match found and address of best result does not match address of searched librarys
 
-"""
-Set of list to show all library results at end of script
-"""
 not_found_libraries = []
-closed_libraries = []
-open_libraries = []
+closed_dict = {}
+open_dict = {}
+wrong_address = {}
 
 def checkIfClosed(library_name, row):
     """
@@ -57,50 +37,67 @@ def checkIfClosed(library_name, row):
             'True': if library is still open
     """
     query_result = google_places.nearby_search(
-        location=str(ws.cell(row=row, column=9).value)+','+str(ws.cell(row=row,column=11).value), keyword=str(library_name),
+        location=str(ws.cell(row=row, column=9).value)+','+str(ws.cell(row=row,column=11).value),
+        keyword=str(library_name)+' '+str(ws.cell(row=row, column=9).value),
         rankby='distance')
     print('Number of results found: ', len(query_result.places))
+    print('that result is: ', query_result.places)
     if len(query_result.places)== 0:
         not_found_libraries.append(library_name)
-        print('no results found')
         return 'not found'
     else:
         place, match = findBestResult(query_result.places, library_name)
         place.get_details()
-        try:
-            print(place.details['permanently_closed'])
-            closed_libraries.append(place.name)
-            return False, match
-        except KeyError:
-            open_libraries.append(place.name)
-            return True, match
+        address = addressChange(place.formatted_address)
+        if addressCheck(address, row):
+            print('address check: True')
+            try:
+                _ = (place.details['permanently_closed'])
+                closed_dict[libary_name]={'found':place.name, 'address':place.formatted_address}
+                return False, match
+            except KeyError:
+                open_dict[library_name]={'found':place.name, 'address':place.formatted_address}
+                return True, match
+        else:
+            print('address check: False')
+            wrong_address[library_name] = {'correct address':str(ws.cell(row=row, column=7).value),
+                                           'found address':place.formatted_address}
+            return 'no match'
 
 def findBestResult(results, library_name):
-    """This function is used to choose best result when
-       more than one result is found. If none of the results
-       match the library name exactly, the most 'prominent'
-       result is returned. 'Prominence' is determined by Google's
-       alogrithms.
-    """
     for place in results:
         if str(place.name) == str(library_name):
             return place, None
         else:
-            print('searched for: ', library_name,)
-            print('found instead: ', place.name)
             pass
-        print('did not find exact match, returning: ', results[0].name,'\n')
-        return results[0], 'notexact'  
+        return results[0], 'notexact'
+
+def addressCheck(address, row):
+    """This function removes numbers from address
+       and returns True if street matches and False
+       if they do not match.
+    """
+    known_address = str(ws.cell(row=row, column =7).value).strip()
+##    print('known address: ', known_address)
+##    print('address: ', address)
+    return address == known_address
+
+def addressChange(address):
+    fixed_address = address[:address.index(',')]
+    return fixed_address
+    
 
 """
 The loop below loops through values in column F (col 6)
 and runs the above function using that value. Depending on the result
 of the function run, the cell color is changed.
 """
-for i in range(2,40): # first number (2) tells script to start on row2, second number tells to stop at row 39. Enter (2,4041) to run all rows.
+for i in range(2,20): # change '40' to '4041' to run script on entire column
     col = 6
     library = (ws.cell(row=i,column=col).value)
     values = checkIfClosed(library,i)
+    if values == 'no match':
+        ws.cell(row=i, column=col).fill=blueFill
     if values[0]==True and values[1]==None:
         ws.cell(row=i,column=col).fill=greenFill
     elif values[0] == False and values[1]==None:
@@ -123,9 +120,17 @@ still open. This may be useful for double checking closed
 libraries or not found libraries. The values which are printed
 in these list reflect the text Google used to search.
 """
-print('These libraries were not found in search:')
-print(not_found_libraries, '\n')
-print('These libraries are permanently closed:')
-print(closed_libraries, '\n')
-print('These libraries are still open:')
-print(open_libraries)
+print('closed dict: ', closed_dict)
+
+print('The following libraries are open')
+for k in open_dict:
+    print('searched for: ', k)
+    print('found: ', open_dict[k]['found'])
+    print('address: ', open_dict[k]['address'])
+
+for k in wrong_address:
+    print('found: ', k)
+    print('found; ', wrong_address[k]['correct address'])
+    print('address: ', wrong_address[k]['found address'])
+
+
